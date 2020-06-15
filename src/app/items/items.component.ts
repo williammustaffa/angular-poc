ded775755031ae8a59d7b8a5185ec3814de047c6
William Lima <william.silva.lima@outlook.com>
2020-06-15 18:07:04 -0300

    Removing and adding items from a list

--- src/app/items/items.component.ts
+++ src/app/items/items.component.ts
@@ -8,10 +8,28 @@ import { Item } from '../item';
 })
 export class ItemsComponent implements OnInit {
   items: Item[] = [
-    { id: 1, name: "test" },
-    { id: 2, name: "test 2" },
-    { id: 3, name: "test 3" },
-  ]
+    { id: 1, name: 'test' },
+    { id: 2, name: 'test 2' },
+    { id: 3, name: 'test 3' },
+  ];
+
+  newItem: string = "";
+
+  addItem(name: string): void {
+    const items = this.items.map(item => item.id);
+    const id = Math.max(...items) + 1;
+
+    this.items.push({
+      id: id,
+      name,
+    });
+
+    this.newItem = '';
+  }
+
+  removeItem(targetItem: Item) {
+    this.items = this.items.filter(item => item.id !== targetItem.id);
+  }
 
   constructor() { }
 
